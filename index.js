05f9c5ba44c16f96edddb92bdf351d7a9a365a76
Thomas Noel <tnoel@novadynamics.com>
2023-12-13 18:01:10 -0800

    Add more boilerplate; implement books

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Put stuff here
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.haveRead = haveRead;
+}
+
+function addBookToLibrary(title, author, pages, haveRead) {
+  myLibrary.push(new Book(title, author, pages, haveRead)); 
+}
+
+function displayLibrary() {
+  myLibrary.forEach((book) => console.log(book));
+}
+
+for (let i = 0; i < 10; i++) {
+  addBookToLibrary(`Book ${i}`, 'the guy', 1000000, true);
+}
+
+displayLibrary();
